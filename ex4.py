b6a4fd69d2562d2e41d86246115a681619d913fb
Dmitry Vasiluk <vasilukwolf@gmail.com>
2018-08-16 21:39:28 +0300

    Variable

--- ex4.py
+++ ex4.py
@@ -4,7 +4,7 @@ drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
-carpool_capacity = cars_driven * space_in_a_car
+carpool_capacity = cars_driven * space_in_a_ca
 average_passengers_per_car = passengers / cars_driven
 print("There are", cars, "cars available.")
 print("There are only", drivers, "drivers available.")
